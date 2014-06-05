b3b37ce81bb55fc4ab38c87d14a4a83bfb002764
stanislav <stanislav@klyukin.note>
2014-06-05 18:26:04 +0400

    comments removed

--- frontend/assets/FrontendAsset.php
+++ frontend/assets/FrontendAsset.php
@@ -1,18 +1,7 @@
 <?php
-/**
- * @link http://www.yiiframework.com/
- * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
- */
-
 namespace frontend\assets;
-
 use yii\web\AssetBundle;
 
-/**
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @since 2.0
- */
 class FrontendAsset extends AssetBundle
 {
 	public $basePath = '@webroot';
