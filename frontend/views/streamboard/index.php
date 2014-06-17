20799338227983233a79be6d52b72952fd3fd974
Andrey <yourwebwizard@gmail.com>
2014-06-18 00:24:56 +0400

    Streamboard initial files

---
+++ frontend/views/streamboard/index.php
@@ -0,0 +1,37 @@
+<!-- // Layout Options Side Panel // -->
+<section id="sidepanel" class="sidepanel">
+	<div class="sidepanel-head">
+		<ul class="nav nav-tabs four-tabs cf">
+			<li class="active"><a href="#general" data-toggle="tab" class="icon-database"></a></li>
+			<li><a href="#design" data-toggle="tab" class="region1"></a></li>
+			<li><a href="#theme" data-toggle="tab" class="region2"></a></li>
+			<li><a href="#settings" data-toggle="tab" class="icon-code"></a></li>
+		</ul>
+	</div>
+	<div class="tab-content">
+		<!-- Accordion slide one (Donations) -->
+		<div class="tab-pane active" id="general">
+			<ul class="bottom-panel-nav two-tabs cf">
+				<li><a>Campaigns</a></li>
+				<li><a>Stats</a></li>
+			</ul>
+		</div>
+		<!-- Accordion slide two (Region 1) -->
+		<div class="tab-pane" id="design">
+			<h3>Region 1</h3>
+		</div>
+		<!-- Accordion slide three (Region 2) -->
+		<div class="tab-pane" id="theme">
+			<h3>Region 2</h3>
+		</div>
+		<!-- Accordion slide three (Region 2) -->
+		<div class="tab-pane" id="settings">
+			<iframe src="http://webdesignermemphis.com/pullrapp/streamboard/source.html" allowtransparency="true"></iframe>
+		</div>
+	</div>
+	<div class="slidepanel">
+		<a class="close icon-cross"></a>
+	</div>
+</section>
+<script src="../js/streamboard.js"></script>
+<script src="../js/plugins.js"></script>
