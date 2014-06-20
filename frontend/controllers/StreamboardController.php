67e612aae0ac5444682144cba47e553661cc0d88
Andrey <yourwebwizard@gmail.com>
2014-06-21 02:11:45 +0400

    streamboard angular controller and layout

--- frontend/controllers/StreamboardController.php
+++ frontend/controllers/StreamboardController.php
@@ -14,14 +14,52 @@ class StreamboardController extends \yii\web\Controller {
         }
 
         $user = Yii::$app->user->identity;
-//        var_dump($user);exit;
 
         $this->layout = 'streamboard';
 
-        return $this->render('index', [
-                    'user' => $user,
-        ]);
 
+
+        $userId = \Yii::$app->user->id;
+        $sql="SELECT * FROM tbl_campaign WHERE userId = '{$userId}'";
+        $campaigns=Yii::$app->db->createCommand($sql)->queryAll();
+//        var_dump($campaigns);exit;
+
+        return $this->render('index', [
+            'user' => $user,
+            'campaigns' => $campaigns,
+            'csrf_token_name' => Yii::$app->request->csrfParam,
+            'csrf_token' => Yii::$app->request->getCsrfToken(),
+        ]);
+    }
+
+    public function actionAdd_donation_ajax() {
+
+    }
+
+    public function actionGet_donations_ajax() {
+        if (Yii::$app->user->isGuest
+//            || !Yii::$app->request->isAjax
+        ) {
+            return false;
+        }
+
+        $launch_time = (int) $_POST['streamboard_launch_time'];
+        var_dump($_POST['campaigns']);exit;
+
+        $userId = \Yii::$app->user->id;
+
+        $sql="SELECT * FROM tbl_donation WHERE userId = '{$userId}'";
+        $donations=Yii::$app->db->createCommand($sql)->queryAll();
+
+
+        $donations = array(
+            array('name'=>'Andrey', 'amount' =>'100'),
+            array('name'=>'Andrey2', 'amount' =>'140'),
+        );
+        if(rand(0,2) > 1) {
+            $donations[] = array('name'=>'Random', 'amount' =>'400');
+        }
+        echo json_encode(array('donations'=>$donations));
     }
 
 }
--- frontend/web/js/streamboard/streamboard.js
+++ frontend/web/js/streamboard/streamboard.js
@@ -12,6 +12,53 @@ $(function () {
         }
     });
 
+    $('ul.bottom-panel-nav li a').click(function() {
+   		$('.'+$(this).data('panel')+'_panel').toggleClass('selected');
+   	});
+    $('a.close').click(function() {
+   		$(this).closest('div').toggleClass('selected');
+   	});
+
 //    $( ".resizable" ).resizable( "option", "maxWidth", '50%' );
+
+});
+
+angular.module('streamboardApp', [])
+.controller('DonationsController', function ($scope, $http) {
+    $scope.donations = [];
+//    $scope.campaigns = [];
+
+    var updateDonations = function () {
+        var campaigns_selected = [];
+        if (typeof $scope.campaigns !== 'undefined') {
+            $.each($scope.campaigns, function( index, value ) {
+                if(String(value) == 'true') {
+                    campaigns_selected.push(index);
+                }
+            });
+        }
+
+        var request_data = {
+            streamboard_launch_time: $scope.start_time,
+            campaigns: campaigns_selected,
+        };
+        request_data[$scope.csrf_token_name] = $scope.csrf_token;
+//        request_data['X-CSRF-Token'] = pub.getCsrfToken();
+
+
+        request_data = $.param(request_data);
+        $http({
+            method: 'POST',
+            url: '/app/streamboard/get_donations_ajax',
+            data: request_data,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).success(function(data) {
+            $scope.donations = data.donations;
+        });
+    };
+    updateDonations();
+    setInterval(function() {
+        $scope.$apply(updateDonations);
+    }, 3000);
 
 });
