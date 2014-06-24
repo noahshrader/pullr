9b679f4aad292551600bf1f646c33e4f57f04752
Stanislav Klyukin <stanislav@klyukin.net>
2014-06-25 01:23:57 +0600

    donor filter with email too

--- frontend/web/js/campaign/donors-filter.js
+++ frontend/web/js/campaign/donors-filter.js
@@ -6,7 +6,10 @@ function donorsWithFilter(donors){
     viewModel.donors = ko.dependentObservable(function() {
         var search = this.query().toLowerCase();
         return ko.utils.arrayFilter(donors, function(donor) {
-            return ( (donor.name.toLowerCase().indexOf(search) >= 0) || (donor.nameFromForm.toLowerCase().indexOf(search) >= 0) );
+            return ( (donor.name.toLowerCase().indexOf(search) >= 0) 
+                    || (donor.nameFromForm.toLowerCase().indexOf(search) >= 0) 
+                    || (donor.email.toLowerCase().indexOf(search) >= 0)
+                    );
         });
     }, viewModel);
 
