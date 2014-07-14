665dadda701d1e4708ac86684ec71946f9e9a7df
Noah Shrader <noahshrader@gmail.com>
2014-07-14 14:56:59 -0400

    scrollbar fix

--- common/web/js/plugins-config.js
+++ common/web/js/plugins-config.js
@@ -2,7 +2,6 @@ $(function () {
     // Custom scrollbars
     $(".campaigns-list-wrap, .site-content").mCustomScrollbar({
         theme:"minimal",
-        setTop: "60px",
         mouseWheel:{preventDefault:true},
         scrollInertia: 0
     });
