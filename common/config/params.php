7bd054388770d498b92ec2b427d22977cf1c19aa
Stanislav Klyukin <stanislav@klyukin.net>
2014-07-23 22:05:36 +0600

    Test environment added

--- common/config/params.php
+++ common/config/params.php
@@ -12,6 +12,9 @@ return [
     /*overright it in params-local.php as params-local is not included in git repository*/
     'paypalClientId' => 'AdhS6hAM2klW0zvrByqMTUAwosKCt8kMrhUPN6-HHzoCaJscFJHsGfGUvLzP',
     'paypalClientSecret' => 'EHYMZhAE29WHfY8T37s-j-2wMOL8SMjjB3uX-9h9uz2snme0pL_tKYLg3YK4',
-    'twitchClientId' => 'l7mj3pfjvxpk2zv6ivr9jpisodqd5h0',
-    'twitchClientSecret' => 'borb565z4l7kzhawe3sb4wufrjfgw4b'
+    /*use that link to add/change your application
+    * "http://www.twitch.tv/settings/connections"
+     */
+    'twitchClientId' => 'remsfof2g40lldoyt81lkldftlis4e7',
+    'twitchClientSecret' => '2xjixm2240k3gj7jwd90hupd9of4kwm'
 ];
