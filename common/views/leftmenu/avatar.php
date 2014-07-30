cf0b4db3f29baea52578624c5abdca7be04d2621
Nikki Schroeher <nikki@thirdshiftdesigns.com>
2014-07-29 22:00:24 -0400

    Avatar Styling

--- common/views/leftmenu/avatar.php
+++ common/views/leftmenu/avatar.php
@@ -7,10 +7,10 @@ use common\components\Application;
 <div class="dropdown avatar-container">
     <ul class="avatar-submenu" role="menu" aria-labelledby="dropdownMenu1">
         <? if (Application::IsBackend()): ?>
-            <li role="presentation"><a role="menuitem" href="admin/logout">Logout</a></li>
+            <li role="presentation"><a role="menuitem" href="admin/logout"><i class="icon-support"></i></a></li>
         <? else: ?>
-            <li role="presentation"><a role="menuitem" href="http://support.pullr.io" target="_blank">Support</a></li>
-            <li role="presentation"><a role="menuitem" href="app/site/logout">Logout</a></li>
+            <li role="presentation"><a role="menuitem" href="http://support.pullr.io" target="_blank"><i class="icon-support"></i></a></li>
+            <li role="presentation"><a role="menuitem" href="app/site/logout"><i class="icon-logout"></i></a></li>
         <? endif; ?>   
     </ul>
 	<div class="avatar dropdown-toggle" data-toggle="dropdown">
