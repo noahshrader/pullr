6b061e527ce860e1fcdfb536c52c02aeb42f9194
Stanislav Klyukin <stanislav@klyukin.net>
2014-08-16 00:45:33 +0600

    angular-bootstrap-select for font-style fixes

--- common/web/js/streamboard/angular-app/regions.js
+++ common/web/js/streamboard/angular-app/regions.js
@@ -2,29 +2,42 @@
     var app = angular.module('pullr.streamboard.regions', ['pullr.common', 'angular-bootstrap-select', 'angular-bootstrap-select.extra']);
     app.run(function ($rootScope, $http) {
         $rootScope.GOOGLE_FONTS = [];
-        $http.get('https://www.googleapis.com/webfonts/v1/webfonts?key='+$rootScope.Pullr.params.googleAPIKey).success(function(data){
-            var items = data.items;
-            var fonts = [];
-            for (var key in items){
-                var item = items[key];
-                fonts.push({name: item.family, value: item.family});
-            }
-            $rootScope.GOOGLE_FONTS = fonts;
-        });
+        var fonts;
+        if (fonts = localStorage.getItem('GOOGLE_FONTS')){
+           $rootScope.GOOGLE_FONTS = JSON.parse(fonts);
+        } else {
+            /*here is float bug can be (rare case, only for first load) - fontStyle directive doesn't rerender after update of GOOGLE_FONTS variable*/
+            $http.get('https://www.googleapis.com/webfonts/v1/webfonts?key='+$rootScope.Pullr.params.googleAPIKey).success(function(data){
+                var items = data.items;
+                var fonts = [];
+                for (var key in items){
+                    var item = items[key];
+                    fonts.push({name: item.family, value: item.family});
+                }
+                localStorage.setItem('GOOGLE_FONTS', JSON.stringify(fonts));
+                $rootScope.GOOGLE_FONTS = fonts;
+            });
+        }
     });
     app.directive('isolatedScope', function () {
         return {
             scope: true
         }
     });
-    app.directive('fontStyle', function ($rootScope, $http) {
+    app.directive('fontStyle', function ($rootScope, $http, $timeout) {
         return {
             scope: {
                 model: '=ngModel'
-//             GOOGLE_FONTS: [{name: 'test', value: 'test'}]
             },
             controller: function ($scope) {
-                $scope.GOOGLE_FONTS = $rootScope.GOOGLE_FONTS;
+                $scope.regionChanged = function(){
+                    /**to have $parent scope updated we are waiting to the next $digest cycle*/
+                    $timeout( function(){
+                        var $parent = $scope.$parent;
+                        var region = $parent.region;
+                        $parent.regionChanged(region);
+                    });
+                }
             },
             templateUrl: 'angular/views/streamboard/region/fontStyle.html'
         }
