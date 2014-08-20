6cc5ebf9c680b16cb5f124ffc603a83dfa39e910
Stanislav Klyukin <stanislav@klyukin.net>
2014-08-20 22:46:22 +0600

    angular regexp filter.

--- common/web/js/streamboard/angular-app/pullr-common.js
+++ common/web/js/streamboard/angular-app/pullr-common.js
@@ -1,8 +1,15 @@
-(function() {
+(function () {
     var app = angular.module('pullr.common', []);
-    app.run(function($rootScope){
+    app.filter('replace', function () {
+            return function (input, regex,replaceTo ) {
+                var pattern = new RegExp(regex,'g');
+                return input.replace(pattern, replaceTo)
+            }
+        }
+    );
+    app.run(function ($rootScope) {
         $rootScope.number_format = number_format;
-        $rootScope.length = function(obj){
+        $rootScope.length = function (obj) {
             return Object.keys(obj).length;
         }
         $rootScope.Pullr = Pullr;
