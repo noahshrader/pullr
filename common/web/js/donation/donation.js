426591b906da3981c5d9499a8e086e4537d1b024
Stanislav Klyukin <stanislav@klyukin.net>
2014-08-26 00:31:08 +0600

    js refactoring  + force update for DonationAsset

--- common/web/js/donation/donation.js
+++ common/web/js/donation/donation.js
@@ -1,101 +1,98 @@
 function donatePageInit() {
-    $("#other-amount").on("change keyup paste",amountChangedEvent);
+    $("#other-amount").on("change keyup paste", amountChangedEvent);
 
     $("#other").hide();
 
-    $('.donationContainer').on('change', 'input[type="radio"].toggle', function() {
+    $('.donationContainer').on('change', 'input[type="radio"].toggle', function () {
         if (this.checked) {
             $('input[name="' + this.name + '"].checked').removeClass('checked');
             $(this).addClass('checked');
         }
     });
-    $('.fieldamount input:radio').click(function() {
+    $('.fieldamount input:radio').click(function () {
         var $els = $('.fieldamount input:radio[name=' + $(this).attr('name') + ']');
         $('.fieldamount input:radio[name=' + $(this).attr('name') + ']').parent().removeClass('active');
         var $active = $(this);
         $active.parent().addClass('active');
-        $els.attr('checked',false);
+        $els.attr('checked', false);
         this.checked = true;
         amountChangedEvent();
     });
     $('.donationContainer input[type="radio"].toggle:checked').addClass('checked');
-    $('a.closethis').click(function() {
+    $('a.closethis').click(function () {
         $('#other').fadeOut(100);
         $('.choice').fadeIn(200);
         $('#otheramount').removeClass('checked');
         $('#option1').attr('checked', 'checked');
         amountChangedEvent();
     });
-    
+
     // Character count for donation form text area
-    $.fn.extend( {
-            limiter: function(limit, elem) {
-                    $(this).on("keyup focus", function() {
+    $.fn.extend({
+        limiter: function (limit, elem) {
+            $(this).each(function(index, value){
+                $(value).on("keyup focus", function () {
                     setCount(this, elem);
                 });
                 function setCount(src, elem) {
-                    if (!src){
-                        return;
-                    }
                     var chars = src.value.length;
                     if (chars > limit) {
                         src.value = src.value.substr(0, limit);
                         chars = limit;
                     }
-                    elem.html( limit - chars );
+                    elem.html(limit - chars);
                 }
-                setCount($(this)[0], elem);
-            }
+                setCount(value, elem);
+            });
+        }
     });
 
-    if($('textarea#donation-comments').length){
-        var elem = $(".counter");
-        $('textarea#donation-comments').limiter(600, elem);
-    }
-    
-    $('#otheramount').click(function(){
-        $('#other').css('display','block');
+    var elem = $(".counter");
+    $('textarea#donation-comments').limiter(600, elem);
+
+    $('#otheramount').click(function () {
+        $('#other').css('display', 'block');
     })
     amountChangedEvent();
 }
 
-function amountChangedEvent(){
-    if ($('.tip-jar').length>0){
-        var value=parseInt($('#other-amount').val());
-        if (!$.isNumeric(value) || value == 0){
+function amountChangedEvent() {
+    if ($('.tip-jar').length > 0) {
+        var value = parseInt($('#other-amount').val());
+        if (!$.isNumeric(value) || value == 0) {
             return;
         }
     } else {
         var value = $('[name=donation-amount]:checked').val();
         var undefinedFlag = false;
-        if (typeof value === 'undefined'){
+        if (typeof value === 'undefined') {
             value = 5;
             undefinedFlag = true;
         }
 
-        if (value=='other'){
-            var value=parseInt($('#other-amount').val());
-            if (!$.isNumeric(value) || value == 0){
+        if (value == 'other') {
+            var value = parseInt($('#other-amount').val());
+            if (!$.isNumeric(value) || value == 0) {
                 return;
             }
         }
     }
-    
+
     $('input#donation-amount').val(value);
-    $('button.donate').text('Donate $'+number_format(value));
-    
+    $('button.donate').text('Donate $' + number_format(value));
+
     /**casting to int*/
-    value=parseInt(value);
-    if (!undefinedFlag){
+    value = parseInt(value);
+    if (!undefinedFlag) {
         var $progressContainer = $('.form-progress');
         var amountRaised = parseInt($progressContainer.data('amountraised'));
         var goalAmount = parseInt($progressContainer.data('goalamount'));
-        amountRaised+=value;
-        $progressContainer.find('.amountRaised').text('$'+number_format(amountRaised));
+        amountRaised += value;
+        $progressContainer.find('.amountRaised').text('$' + number_format(amountRaised));
         /*preventing division by zero*/
         goalAmount = Math.max(1, goalAmount);
-        var percent = 100*amountRaised/goalAmount;
-        $progressContainer.find('.progress').css('width',percent+'%');
+        var percent = 100 * amountRaised / goalAmount;
+        $progressContainer.find('.progress').css('width', percent + '%');
     }
 }
 
