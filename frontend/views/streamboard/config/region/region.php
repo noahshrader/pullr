b1168f3cecb7c5fc0eab8d32fb78a589d5c88fea
Noah Shrader <noahshrader@gmail.com>
2014-09-02 00:48:36 -0500

    select tweaks

--- frontend/views/streamboard/config/region/region.php
+++ frontend/views/streamboard/config/region/region.php
@@ -15,8 +15,9 @@ $typesList = [
             <h3>Region {{ region.regionNumber}} <input type="color" ng-model="region.backgroundColor" ng-change="regionChanged(region)" class="color-choice"></h3>
         </div>
         <div class="form-group" ng-init="WIDGET_TYPES=<?= htmlspecialchars(json_encode($typesList)) ?>">
-            <select ui-select2="{minimumResultsForSearch: -1}" ng-model="region.widgetType" ng-change="regionChanged(region)" ng-options="type.value as type.name for type in WIDGET_TYPES" data-placeholder="Select one...">
+            <select ui-select2="{minimumResultsForSearch: -1}" ng-model="region.widgetType" ng-change="regionChanged(region)" data-placeholder="Select one...">
                 <option value=""></option>
+                <option ng-repeat="type in WIDGET_TYPES" value="{{type.value}}">{{type.name}}</option>
             </select>
         </div>
         <div class="widgetContainer">
