acdec91df2090fd70a42b9ebe02a88a8cfbd6fca
Stanislav Klyukin <stanislav@klyukin.net>
2014-10-03 03:08:19 +0600

    timers update

--- common/web/js/streamboard/angular-app/regionsPanels.js
+++ common/web/js/streamboard/angular-app/regionsPanels.js
@@ -1,90 +1,103 @@
 (function () {
     var app = angular.module('pullr.streamboard.regionsPanels', ['pullr.streamboard.stream',
-        'pullr.streamboard.regions', 'pullr.streamboard.alertMediaManager', 'pullr.streamboard.donations',
-        'pullr.streamboard.campaigns', 'pullr.currentTime', 'timer']);
-    app.controller('RegionsCtrl', function ($scope, stream, regions, $interval, alertMediaManager, donations, campaigns) {
-        $scope.streamService = stream;
-        $scope.regionsService = regions;
-        $scope.donationsService = donations;
-        $scope.campaignsService = campaigns;
+            'pullr.streamboard.regions', 'pullr.streamboard.alertMediaManager', 'pullr.streamboard.donations',
+            'pullr.streamboard.campaigns', 'pullr.currentTime', 'timer']).
+        controller('RegionsCtrl', function ($scope, stream, regions, $interval, alertMediaManager, donations, campaigns) {
+            $scope.streamService = stream;
+            $scope.regionsService = regions;
+            $scope.donationsService = donations;
+            $scope.campaignsService = campaigns;
 
-        $scope.regionsService.ready(function () {
-            requireAllFonts();
-            /*whenever regions are changes we are checking that we have right fonts*/
-            $scope.$watch('regionsService.regions', requireAllFonts, true);
+            $scope.regionsService.ready(function () {
+                requireAllFonts();
+                /*whenever regions are changes we are checking that we have right fonts*/
+                $scope.$watch('regionsService.regions', requireAllFonts, true);
+                $scope.$watch('regionsService.regions', updateTimestamps, true);
 
-            /*we make a delay as streamboard may still be loading, even if regions are ready*/
-            $interval(function () {
-                $.each($scope.regionsService.regions, function (index, region) {
-                    /*creating namespace for showing data*/
-                    region.toShow = {alert: {
-                    }};
-                    $interval(function () {
-                        showAlert(region)
-                    }, 1, 1);
-                });
-            }, 4000, 1);
-        })
-
-        function requireAllFonts(){
-            for (var key in regions.regions){
-                var region = regions.regions[key];
-                window.requireGoogleFont(region.widgetAlerts.followersPreference.fontStyle);
-                window.requireGoogleFont(region.widgetAlerts.donationsPreference.fontStyle);
-                window.requireGoogleFont(region.widgetAlerts.subscribersPreference.fontStyle);
-                window.requireGoogleFont(region.widgetDonationFeed.fontStyle);
-                window.requireGoogleFont(region.widgetCampaignBar.fontStyle);
-            }
-        }
-        function capitaliseFirstLetter(string)
-        {
-            return string.charAt(0).toUpperCase() + string.slice(1);
-        }
-
-        function showAlert(region) {
-            var stream = $scope.streamService.streams[region.regionNumber];
-            var notification = false;
-            if (region.widgetType == 'widget_alerts') {
-                while (stream.length > 0 && notification == false) {
-                    notification = stream.shift();
-                    /**checking includeDonations, includeFollowers, includeSubscribers*/
-                    if (!region.widgetAlerts['include'+capitaliseFirstLetter(notification.type)]){
-                        notification = false;
-                    }
-                }
-            } else {
-                $scope.streamService.streams[region.regionNumber] = [];
-            }
-
-            if (notification) {
-                console.log(['WE HAVE NOTIFICATION FOR REGION ' + region.regionNumber]);
-                console.log(notification);
-                var toShow = region.toShow.alert;
-                toShow.message = notification.message;
-                toShow.preference = region.widgetAlerts[notification.type + 'Preference'];
-                window.requireGoogleFont(toShow.preference.fontStyle);
-
-                var preference = toShow.preference;
-                toShow.image = alertMediaManager.getImageUrl(preference.image, preference.imageType);
-
-                alertMediaManager.playSound(preference.sound, preference.soundType, preference.volume);
-
+                /*we make a delay as streamboard may still be loading, even if regions are ready*/
                 $interval(function () {
-                    /**@todo check animationDuration*/
-                    hideAlert(region);
-                }, preference.animationDuration * 1000, 1);
-            } else {
+                    $.each($scope.regionsService.regions, function (index, region) {
+                        /*creating namespace for showing data*/
+                        region.toShow = {alert: {
+                        }};
+                        $interval(function () {
+                            showAlert(region)
+                        }, 1, 1);
+                    });
+                }, 4000, 1);
+            });
+
+            $interval(function () {
+                $scope.currentTimestamp = new Date().getTime();
+            }, 1000);
+
+            function updateTimestamps() {
+                for (var key in regions.regions) {
+                    var region = regions.regions[key];
+                    var module = region.widgetCampaignBar.timerModule;
+                    module.countDownFromTimestamp = new Date(module.countDownFrom).getTime();
+                }
+            }
+
+            function requireAllFonts() {
+                for (var key in regions.regions) {
+                    var region = regions.regions[key];
+                    window.requireGoogleFont(region.widgetAlerts.followersPreference.fontStyle);
+                    window.requireGoogleFont(region.widgetAlerts.donationsPreference.fontStyle);
+                    window.requireGoogleFont(region.widgetAlerts.subscribersPreference.fontStyle);
+                    window.requireGoogleFont(region.widgetDonationFeed.fontStyle);
+                    window.requireGoogleFont(region.widgetCampaignBar.fontStyle);
+                }
+            }
+
+            function capitaliseFirstLetter(string) {
+                return string.charAt(0).toUpperCase() + string.slice(1);
+            }
+
+            function showAlert(region) {
+                var stream = $scope.streamService.streams[region.regionNumber];
+                var notification = false;
+                if (region.widgetType == 'widget_alerts') {
+                    while (stream.length > 0 && notification == false) {
+                        notification = stream.shift();
+                        /**checking includeDonations, includeFollowers, includeSubscribers*/
+                        if (!region.widgetAlerts['include' + capitaliseFirstLetter(notification.type)]) {
+                            notification = false;
+                        }
+                    }
+                } else {
+                    $scope.streamService.streams[region.regionNumber] = [];
+                }
+
+                if (notification) {
+                    console.log(['WE HAVE NOTIFICATION FOR REGION ' + region.regionNumber]);
+                    console.log(notification);
+                    var toShow = region.toShow.alert;
+                    toShow.message = notification.message;
+                    toShow.preference = region.widgetAlerts[notification.type + 'Preference'];
+                    window.requireGoogleFont(toShow.preference.fontStyle);
+
+                    var preference = toShow.preference;
+                    toShow.image = alertMediaManager.getImageUrl(preference.image, preference.imageType);
+
+                    alertMediaManager.playSound(preference.sound, preference.soundType, preference.volume);
+
+                    $interval(function () {
+                        /**@todo check animationDuration*/
+                        hideAlert(region);
+                    }, preference.animationDuration * 1000, 1);
+                } else {
+                    $interval(function () {
+                        showAlert(region);
+                    }, 100, 1);
+                }
+            }
+
+            function hideAlert(region) {
+                region.toShow.alert = {};
                 $interval(function () {
                     showAlert(region);
-                }, 100, 1);
+                }, 1000, 1)
             }
-        }
-
-        function hideAlert(region) {
-            region.toShow.alert = {};
-            $interval(function () {
-                showAlert(region);
-            }, 1000, 1)
-        }
-    });
+        });
 })()
--- frontend/views/streamboard/region/campaign-bar/timer.php
+++ frontend/views/streamboard/region/campaign-bar/timer.php
@@ -3,13 +3,19 @@ use frontend\models\streamboard\WidgetCampaignBarTimer;
 
 ?>
 <div ng-if="region.widgetCampaignBar.timerEnable" child-scope ng-init="module = region.widgetCampaignBar.timerModule">
-    <div >
+    <div>
         <div ng-if="module.timerType == '<?= WidgetCampaignBarTimer::TIMER_TYPE_LOCALTIME ?>' ">
             Current time: <span current-time format="h:mma"></span>
         </div>
         <div ng-if="module.timerType == '<?= WidgetCampaignBarTimer::TIMER_TYPE_COUNTDOWN ?>' ">
-            text {{module.countDownTo}}
-            <timer>{{module.countDownTo}}</timer>
+            <div ng-show="currentTimestamp>module.countDownFromTimestamp">
+                <timer end-time="module.countDownTo">
+                    <span ng-show="days">{{days}} day{{daysS}},</span>
+                    <span ng-show="hours || days">{{hours}} hour{{hoursS}},</span>
+                    <span ng-show="minutes || hours || days">{{minutes}} minute{{minutesS}},</span>
+                    <span>{{seconds}} second{{secondsS}}.</span>
+                </timer>
+            </div>
         </div>
     </div>
 </div>
