86e62b4416063410b6306fe779b341554f926d28
Andy Chipperfield <hi@andychip.com>
2014-10-14 19:16:21 +0100

    Adding the first of the themes

--- frontend/web/themes/bdsingle/js/fitvids.js
+++ frontend/web/themes/bdsingle/js/fitvids.js
@@ -1,5 +1,13 @@
-/* start fitvids */
-
+/*global jQuery */
+/*jshint browser:true */
+/*!
+* FitVids 1.1
+*
+* Copyright 2013, Chris Coyier - http://css-tricks.com + Dave Rupert - http://daverupert.com
+* Credit to Thierry Koblentz - http://www.alistapart.com/articles/creating-intrinsic-ratios-for-video/
+* Released under the WTFPL license - http://sam.zoy.org/wtfpl/
+*
+*/
 
 (function( $ ){
 
@@ -73,6 +81,3 @@
   };
 // Works with either jQuery or Zepto
 })( window.jQuery || window.Zepto );
-
-
-/* end fitvids */
---
+++ frontend/web/themes/blsingle/manifest.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'name' => 'Light Single Stream One',
+    'description' => 'Light Single Stream',
+    'layoutType' => 'Twitch Single Player',
+    'plan' => 'Basic'
+];
