7e2230ee6c3cc3191f3f6f1a4335405c6337637d
Noah Shrader <noahshrader@gmail.com>
2014-10-21 16:27:21 -0500

    theme improvements

--- frontend/web/global/themes/js/teamstream.js
+++ frontend/web/global/themes/js/teamstream.js
@@ -1,5 +1,12 @@
 var myCustomAfterRender = function(){}
 $(function(){
 	myCustomAfterRender = function(){
+		$('.togglechat').click(function (e) {			
+			$(this).toggleClass('chaton');
+		  	$('.featuredchat').fadeToggle();
+		  	$('.featuredstreamcontainer').toggleClass('biggerFeaturedStream');
+		  	e.preventDefault();
+		});
+		$("#stream").fitVids();
 	}
 });
