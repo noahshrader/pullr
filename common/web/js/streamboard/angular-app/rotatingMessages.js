8c0e38ceb492541afbe5d83dba7e358e5d8392f9
Nam Tran <nam.trankhanh.vn@gmail.com>
2014-10-23 10:36:09 +0700

    fix bug on rotating message when all messages are emtpy

--- common/web/js/streamboard/angular-app/rotatingMessages.js
+++ common/web/js/streamboard/angular-app/rotatingMessages.js
@@ -5,25 +5,56 @@
             function link(scope, element, attrs) {
              
                 var $messageContainer = $('#rotating-message-container');
-                var isRunning = false;
-                var messagesModule = null,
+                var isRunning = false,
+                
                     i = -1,
           
                     rotationSpeed = 5;
-
+                var timer;
+                var messagesModule;
                 scope.$watch(attrs.rotationSpeed, function (value) {
                     rotationSpeed = value;
                 });
-                scope.$watch(attrs.messagesModule, function (value) {
-                    messagesModule = value;
+                scope.$watch('messagesModule', function (value) {
+                    messagesModule = value;              
+                    if (messagesModule) {
+                        if ( ! isRunning ) {
+                            isRunning = true;
+                            nextMessage();
+                        }                        
+                    } else {
+                        isRunning = false;
+                    } 
                     if ( ! isRunning ) {
+                        $interval.cancel(timer);
+                    }                   
+                });
+
+                scope.$watchCollection('[messagesModule.message1, messagesModule.message2, messagesModule.message3, messagesModule.message4, messagesModule.message5]', function(messages){
+                    var allMessageIsEmpty = true;
+                    for (var i = 0; i < messages.length; i++) {
+                        if (messages[i].trim() != '') {
+                            allMessageIsEmpty = false;
+                            break;
+                        }
+                    }        
+                    if (allMessageIsEmpty) {
+                        isRunning = false;
+                        console.log('[Rorating messages]: Empty message list. Stop');
+                    } else if (messagesModule && ! isRunning) {
                         isRunning = true;
                         nextMessage();
                     }
-                });
+                    if ( ! isRunning ) {
+                        $interval.cancel(timer);
+                    }    
+                })
+
+                
 
                 element.on('$destroy', function () {
-                    isRunning = false;
+                    isRunning = false; 
+                    $interval.cancel(timer);                                   
                 });      
 
                 function getMessage(i) {
@@ -48,19 +79,18 @@
                     while ((!getMessage(i)) && j < 5) {
                         i = (i + 1) % 5;
                         j++;
-                    }
-                    console.log(getMessage(i));
-                    if (getMessage(i)) {                        
+                    }            
+                    if (getMessage(i)) {                         
                         $messageContainer.html(getMessage(i));
                         $messageContainer.removeClass().addClass('animated fadeIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-                            $interval(function(){
+                            timer = $interval(function(){
                                 $messageContainer.removeClass().addClass('animated fadeOut');
                                 if( isRunning ) {
                                     $messageContainer.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', nextMessage);                                
                                 }  
                             }, rotationSpeed * 1000, 1)                                          
                         });                        
-                    }
+                    } 
 
                   
                     
@@ -68,6 +98,9 @@
             }
 
             return {
+                scope: {
+                    messagesModule:'='
+                },
                 compile: function(tElement) {
                     var $element = $(tElement);
                     $element.append('<div id="rotating-message-container"></div>');
