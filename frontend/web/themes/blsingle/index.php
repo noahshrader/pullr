a2e9e2ea1c8ae967a67bd1e6c2f821a62c1082ae
Noah Shrader <noahshrader@gmail.com>
2014-10-24 02:41:46 -0500

    theme improvements

--- frontend/web/themes/blsingle/index.php
+++ frontend/web/themes/blsingle/index.php
@@ -103,6 +103,11 @@
 <!-- Layout -->
 <div pullr-campaign-layout></div>
 
+<!-- Description -->
+<section class="campaign-description container">
+	<p>{{campaign.description}}</p>
+</section>
+
 <!-- Footer -->
 <footer>
 	<h5>Powered by</h5>
