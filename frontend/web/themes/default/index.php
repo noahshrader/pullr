d6d10abcaf41a8236777fde55fee08e890387a28
Aleksandr Fedotov <trerums@gmail.com>
2014-10-24 07:45:33 +0000

    Themes tweaks to handle $0 goalAmount properly

--- frontend/web/themes/default/index.php
+++ frontend/web/themes/default/index.php
@@ -47,9 +47,9 @@
             <span>Amount Raised</span>
             <h1 style="color:{{campaign.primaryColor}};">{{campaign.amountRaisedFormatted}}</h1>
         </div>
-        <div class="col-md-4 col-sm-4 col-xs-4 center campaign-goal">
-            <span>Goal Amount</span>
-            <h1>{{campaign.goalAmountFormatted}}</h1>
+        <div class="col-md-4 col-sm-4 col-xs-4 center">
+            <span ng-hide='campaign.goalAmount == 0.00'>Goal Amount</span>
+            <h1 ng-hide='campaign.goalAmount == 0.00'>{{campaign.goalAmountFormatted}}</h1>
         </div>
         <div class="col-md-4 col-sm-4 col-xs-4 right">
             <span>Donors</span>
