26acb36342d29cba03e8ad04223d8cdad9c2fc88
Noah Shrader <noahshrader@gmail.com>
2014-10-25 13:16:29 -0500

    streamboard tweak

--- frontend/views/streamboard/region/regions.php
+++ frontend/views/streamboard/region/regions.php
@@ -11,13 +11,11 @@ $class = 'regionsNumber' . $regionsNumber;
 <div class="regionsContainer <?= $class ?>" ng-controller="RegionsCtrl">
     <div class="region resizable-v" ng-repeat="region in regionsService.regions"
          ng-style="{'background-color': region.backgroundColor}">
-        <div ng-show="region.toShow.alert.message && (region.widgetType == 'widget_alerts')" class="widget-alerts" draggable position-container="region.widgetAlerts" region="region">
+        <div ng-show="region.toShow.alert.message && (region.widgetType == 'widget_alerts')" class="widget-alerts" position-container="region.widgetAlerts" region="region">
             <!-- if alert message is showed-->
-            <img ng-src="{{region.toShow.alert.image}}" ng-hide='region.toShow.alert.preference.hideAlertImage'>
+            <img ng-src="{{region.toShow.alert.image}}" ng-hide='region.toShow.alert.preference.hideAlertImage' draggable>
 
-            <div
-                ng-style="{'color': region.toShow.alert.preference.fontColor, 'font-size': region.toShow.alert.preference.fontSize, 'font-family': region.toShow.alert.preference.fontStyle}"
-                ng-hide="region.toShow.alert.preference.hideAlertText">
+            <div ng-style="{'color': region.toShow.alert.preference.fontColor, 'font-size': region.toShow.alert.preference.fontSize, 'font-family': region.toShow.alert.preference.fontStyle}" ng-hide="region.toShow.alert.preference.hideAlertText" draggable>
                 {{region.toShow.alert.message}}
             </div>
         </div>
