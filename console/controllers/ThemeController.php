ab7df58356ddb4a8a08a7999e7fac48b65040be3
Thuong <dinhthuong02@hotmail.com>
2014-11-04 09:37:08 +0700

    update default theme for layout

--- console/controllers/ThemeController.php
+++ console/controllers/ThemeController.php
@@ -12,5 +12,13 @@ class ThemeController extends Controller {
         $scanner = new ThemeScanner();
         $scanner->rescan();
         echo "Themes were rescaned\n";
+        
+        echo 'Updating theme default value';
+        
+        \Yii::$app->db->createCommand('UPDATE '. \common\models\Theme::tableName() .' SET is_default = 1 WHERE `filename` = \'bdmulti-1\' ')->execute();
+        \Yii::$app->db->createCommand('UPDATE '. \common\models\Theme::tableName() .' SET is_default = 1 WHERE `filename` = \'bdsingle\' ')->execute();
+        \Yii::$app->db->createCommand('UPDATE '. \common\models\Theme::tableName() .' SET is_default = 1 WHERE `filename` = \'bdteam-1\' ')->execute();
+        
+        echo 'Updated theme default value';
     }
 }
