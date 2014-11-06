084f3cb6d9f8b894dbadb963181622acd6e56150
Noah Shrader <noahshrader@gmail.com>
2014-11-06 13:04:49 -0600

    default theme fix

--- frontend/web/themes/default/index.php
+++ frontend/web/themes/default/index.php
@@ -1,4 +1,4 @@
-<!doctype html>
+\<!doctype html>
 <!--[if lt IE 7]> <html lang="en-us" class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>    <html lang="en-us" class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>    <html lang="en-us" class="no-js lt-ie9"> <![endif]-->
@@ -18,7 +18,7 @@
     <!-- Theme Specific -->
     <link rel="stylesheet" href="/themes/bdsingle/css/master.css" />
     <!-- Scripts -->
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script> 
     <script src="//ajax.aspnetcdn.com/ajax/jquery.templates/beta1/jquery.tmpl.min.js"></script> 
     <script src="api/script"></script>
     <script src="/global/themes/global.js"></script>
@@ -27,97 +27,115 @@
     <!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
 <body class="responsive" id="top">
-
-<!-- Header -->
-<header>
-    <div class="container">
-        <div class="logo">
-            <a href="/" class="icon-pullr logo"></a>
-        </div>
-        <div class="menu-actions">
-            <a href="http://www.pullr.io">Start your campaign</a>
-        </div>
-    </div>
-</header>
-
-<!-- Main Stats -->
-<section class="stats" bg-image="{{campaign.backgroundImg}}">
-    <div class="numbers container" ng-cloak>
-        <div class="col-md-4 col-sm-4 col-xs-4 raised">
-            <span>Amount Raised</span>
-            <h1 style="color:{{campaign.primaryColor}};">{{campaign.amountRaisedFormatted}}</h1>
-        </div>
-        <div class="col-md-4 col-sm-4 col-xs-4 center">
-            <span ng-hide='campaign.goalAmount == 0.00'>Goal Amount</span>
-            <h1 ng-hide='campaign.goalAmount == 0.00'>{{campaign.goalAmountFormatted}}</h1>
-        </div>
-        <div class="col-md-4 col-sm-4 col-xs-4 right">
-            <span>Donors</span>
-            <h1>{{campaign.numberOfUniqueDonors}}</h1>
+<div class="donation-form">
+    <iframe src="<?= yii\helpers\Url::to().'/donate' ?>"></iframe>
+</div>
+<div class="wrapper">
+    <!-- Slidestats -->
+    <div class="slidestats">
+        <div class="container">
+            <h5 class="campaign">{{campaign.name}}</h5>
+            <h5 class="amount-raised" style="color:{{campaign.primaryColor}};"><i class="icon icon-coin"></i>{{campaign.amountRaisedFormatted}}</h5>
         </div>
     </div>
-    <div class="amount-progress">
-        <div class="project-progress status">
-            <div class="project-progressbar" style="background:{{campaign.primaryColor}}; width: {{campaign.percentageOfGoal}}%"></div>
-        </div>
-    </div>
-</section>
-
-<!-- User -->
-<section class="user-bar">
-    <div class="container">
-        <div class="user"> 
-            <div class="avatar" ng-show='campaign.layoutType == LAYOUT_TYPE_SINGLE || campaign.layoutType == LAYOUT_TYPE_MULTI'>                
-                <img src="{{channel.logo}}" ng-cloak/>
-                <a href="http://www.twitch.tv/{{channel.display_name}}" ng-cloak>{{channel.display_name}}</a>
+    <!-- Header -->
+    <header>
+        <div class="container">
+            <div class="logo">
+                <a href="/" class="icon-pullr logo"></a>
             </div>
-            <div class="avatar" ng-show='campaign.layoutType == LAYOUT_TYPE_TEAM'>                
-                <img src="{{team.logo}}" ng-cloak/>
-                <a href="http://www.twitch.tv/{{campaign.channelTeam}}" ng-cloak>{{team.display_name}}</a>
+            <div class="menu-actions">
+                <a href="http://www.pullr.io">Start your campaign</a>
             </div>
+        </div>
+    </header>
 
+    <!-- Main Stats -->
+    <section class="stats" bg-image="{{campaign.backgroundImg}}">
+        <div class="stats-wrap">
+            <div class="campaign-info">
+                <h1 ng-cloak>{{campaign.name}}</h1>
+                <h4 style="color:{{campaign.primaryColor}}" ng-cloak>{{campaign.charity.name}}</h4>
+            </div>
+            <div class="numbers" ng-cloak>
+                <div class="container">
+                    <div class="col-md-4 col-sm-4 col-xs-4 raised">
+                        <span>Amount Raised</span>
+                        <h1 style="color:{{campaign.primaryColor}};">{{campaign.amountRaisedFormatted}}</h1>
+                    </div>
+                    <div class="col-md-4 col-sm-4 col-xs-4 center">
+                        <span ng-hide='campaign.goalAmount == 0.00'>Goal Amount</span>
+                        <h1 ng-hide='campaign.goalAmount == 0.00'>{{campaign.goalAmountFormatted}}</h1>
+                    </div>
+                    <div class="col-md-4 col-sm-4 col-xs-4 right">
+                        <span>Donors</span>
+                        <h1>{{campaign.numberOfUniqueDonors}}</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="project-progress status">
+                <div class="project-progressbar" style="background:{{campaign.primaryColor}}; width: {{campaign.percentageOfGoal}}%"></div>
+            </div>
+        </div>
+        <div class="overlay"></div>
+    </section>
+
+    <!-- User -->
+    <section class="user-bar">
+        <div class="container">
+            <div class="user">
+                <div class="avatar">
+                    <img src="{{channel.logo}}" ng-cloak/>
+                    <a href="http://www.twitch.tv/{{channel.display_name}}" ng-cloak>{{channel.display_name}}</a>
+                </div>
+                <div class="user-details">
+                    <span ng-show='campaign.layoutType == LAYOUT_TYPE_MULTI || campaign.layoutType == LAYOUT_TYPE_SINGLE'><i class="icon icon-user" ng-cloak></i>{{selectedChannel.followers}}</span>
+                    <span ng-show='campaign.layoutType == LAYOUT_TYPE_MULTI || campaign.layoutType == LAYOUT_TYPE_SINGLE'><i class="icon icon-view" ng-cloak></i>{{selectedChannel.views}}</span>
+                </div>
+            </div>
             <div class="user-details-social">
                 <a href="http://www.twitch.tv/{{channel.display_name}}" title="Twitch" class="icon-twitch2"></a>
                 <a href="http://twitter.com/{{campaign.twitterName}}" class="icon-twitter" title="Twitter" ng-show="campaign.twitterEnable == 1"></a>
-                <a href="{{campaign.facebookUrl}}" class="icon-facebook" title="Facebook" ng-show="campaign.facebookUrl == 1"></a>
-                <a href="{{campaign.youtubeUrl}}" class="icon-youtube" title="YouTube" ng-show="campaign.youtubeEnable == 1"></a>
+                    <a href="{{campaign.facebookUrl}}" class="icon-facebook" title="Facebook" ng-show="campaign.facebookUrl == 1"></a>
+                    <a href="{{campaign.youtubeUrl}}" class="icon-youtube" title="YouTube" ng-show="campaign.youtubeEnable == 1"></a>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 
-<!-- Campaign Info -->
-<section class="info container cf" ng-cloak>
-    <div class="container" ng-cloak>
-        <div class="col-md-4 col-sm-4 col-xs-12 right donate-wrap">
-            <button href="<?= yii\helpers\Url::to().'/donate' ?>" class="btn btn-primary donate" data-effect="mfp-zoom-in" style="background:{{campaign.primaryColor}}">Donate!</button>
-        </div>
-        <div class="col-md-6 col-sm-6 col-xs-8 feed-details">
-            <div class="feed-details-title">
-                <h5>
-                    <span data-pullr='campaign-startDateFormatted' ng-cloak>{{campaign.startDateFormatted}}</span> - <span data-pullr='campaign-endDateFormatted'>{{campaign.endDateFormatted}}</span>
-                </h5>
-                <h2>{{campaign.name}}</h2>
-                <h4 style="color:{{campaign.primaryColor}}">{{campaign.charity.name}}</h4>
+    <!-- Campaign Info -->
+    <section class="info container" ng-cloak>
+        <div class="info-wrap cf">
+            <div class="right donate-wrap">
+                <div class="donate-button">
+                    <button class="btn btn-primary donate" style="background:{{campaign.primaryColor}}">Donate</button>
+                    <div class="slant"></div>
+                </div>
+            </div>
+            <div class="feed-details">
+                <h4 ng-show='campaign.layoutType == LAYOUT_TYPE_MULTI || campaign.layoutType == LAYOUT_TYPE_SINGLE'>{{selectedChannel.status}}</h4>
+                <span ng-show='campaign.layoutType == LAYOUT_TYPE_MULTI || campaign.layoutType == LAYOUT_TYPE_SINGLE'>{{selectedChannel.game}}</span>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 
-<!-- Layout -->
-<div pullr-campaign-layout></div>
+    <!-- Layout -->
+    <div pullr-campaign-layout></div>
 
-<!-- Footer -->
-<footer>
-    <h5>Powered by</h5>
-    <a class="logo icon-pullr" href="http://www.pullr.io" target="_blank"></a>
-    <span class="copy"> &copy; <? print(Date("Y")); ?> Pullr, LLC. All Rights Reserved. <a href="http://www.pullr.io/terms">Terms &amp; Conditions</a> &bull; <a href="http://www.pullr.io/privacy">Privacy Policy</a></span>
-</footer>
+    <!-- Description -->
+    <section class="campaign-description container">
+        <p>{{campaign.description}}</p>
+    </section>
 
+    <!-- Footer -->
+    <footer>
+        <h5>Powered by</h5>
+        <a class="logo icon-pullr" href="http://www.pullr.io" target="_blank"></a>
+        <span class="copy"> &copy; <? print(Date("Y")); ?> Pullr, LLC. All Rights Reserved. <a href="http://www.pullr.io/terms">Terms &amp; Conditions</a> &bull; <a href="http://www.pullr.io/privacy">Privacy Policy</a></span>
+    </footer>
+</div>
 <script type='text/javascript'>
     Pullr.Init({id: <?= $campaign->id ?>, key: <?= json_encode($campaign->key) ?>});
-    // Pullr.Ready(function(){alert(Pullr.event.name)});
 </script>
-<script src="/global/themes/js/singlestream.js"></script>
+<script src="/global/themes/js/stream.js"></script>
 </body>
 </html>
