c9d7455125a421841e2618fd60de8fc9bf1dac7d
Noah Shrader <noahshrader@gmail.com>
2014-11-06 16:23:41 -0600

    donation form tab cleanup

--- frontend/views/campaigns/campaign-edit-form.php
+++ frontend/views/campaigns/campaign-edit-form.php
@@ -5,35 +5,29 @@
 <div id="campaign-edit-form">
     <div class="module-inner">
         <h5><i class="icon icon-coin"></i>General</h5>
-        <!-- Form Visibility -->
-        <div class="form-group field-campaign-formvisibility">
-                <label>Form Visibility</label><i class="icon icon-help" data-toggle="tooltip" data-placement="right" title="Select 'Visible' if you want to show your form. Select 'Hidden' to hide your form."></i>
-                
-                <? $keyValues = [ true => 'Visible', false => 'Hidden']; ?>
-                <?= Html::activeDropDownList($campaign, 'formVisibility', $keyValues, ['class' => 'select-block']) ?>
-        </div>
-
-        <?=$form->field($campaign, 'donationButtonText')->input('text'); ?>
-
         <div class="row">
-	        <div class="col-sm-4">
-		        <!-- Donor Comments -->
-		        <div class="form-group">
-		            <?= $form->field($campaign, 'enableDonorComments', ['autoPlaceholder' => false])->label("Donor Comments")->checkbox([], false); ?>
-		        </div>
-	        </div>
-	        <div class="col-sm-4">
-		        <!-- Enable Thank You Page -->
-		        <div class="form-group">
-		            <?= $form->field($campaign, 'enableThankYouPage', ['autoPlaceholder' => false])->label("Thank You Page")->checkbox([], false); ?>
-		        </div>
-	        </div>
+            <div class="col-sm-4">
+                <!-- Donor Comments -->
+                <div class="form-group">
+                    <?= $form->field($campaign, 'enableDonorComments', ['autoPlaceholder' => false])->label('Donor Comments <i class="icon icon-help" data-toggle="tooltip" data-placement="right" title="Toggle to off to disallow donor comments on your donation form."></i>')->checkbox([], false); ?>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <!-- Enable Thank You Page -->
+                <div class="form-group">
+                    <?= $form->field($campaign, 'enableThankYouPage', ['autoPlaceholder' => false])->label('Thank You Page <i class="icon icon-help" data-toggle="tooltip" data-placement="right" title="Toggle to off to disallow users from redirecting to your thank you page after donating."></i>')->checkbox([], false); ?>
+                </div>
+            </div>
             <div class="col-sm-4">
                 <!-- Enable\disable donation form progress bar -->
                 <div class="form-group">
-                    <?= $form->field($campaign, 'enableDonationProgressBar', ['autoPlaceholder' => false])->label("Progress Bar")->checkbox([], false); ?>
+                    <?= $form->field($campaign, 'enableDonationProgressBar', ['autoPlaceholder' => false])->label('Progress Bar <i class="icon icon-help" data-toggle="tooltip" data-placement="right" title="Toggle to off to hide the progress bar on your donation form."></i>')->checkbox([], false); ?>
                 </div>
             </div>
+        </div>
+        <!-- Donate Button Text -->
+        <div class="form-group">
+            <?=$form->field($campaign, 'donationButtonText', ['autoPlaceholder' => false])->label('Donate Button Text <i class="icon icon-help" data-toggle="tooltip" data-placement="right" title="Have your donate buttons say something else."></i>')->input('text'); ?>
         </div>
         <!-- Custom Thank You HTML -->
         <div class="form-group">
@@ -41,6 +35,13 @@
                 'options' => ['rows' => 6],
                 'preset' => 'basic'
             ]) ?>
+        </div>
+        <!-- Form Visibility -->
+        <div class="form-group field-campaign-formvisibility">
+                <label>Form Visibility</label><i class="icon icon-help" data-toggle="tooltip" data-placement="right" title="Select 'Visible' if you want to show your form. Select 'Hidden' to hide your form."></i>
+                
+                <? $keyValues = [ true => 'Visible', false => 'Hidden']; ?>
+                <?= Html::activeDropDownList($campaign, 'formVisibility', $keyValues, ['class' => 'select-block']) ?>
         </div>
     </div>
 </div>
