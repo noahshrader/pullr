567b2bc7f424649d294a03ef89295be0e4f8a21a
Aleksandr Fedotov <trerums@gmail.com>
2014-11-06 20:13:35 +0000

    Single theme needs to be the default selection with the username initially selected

--- frontend/views/campaigns/campaign-edit-layout.php
+++ frontend/views/campaigns/campaign-edit-layout.php
@@ -19,7 +19,7 @@ $user = \Yii::$app->user->identity;
 
         <!-- if Single Channel -->
         <div id="campaign-channelname" class="form-group highlight-wrap">
-            <?= $form->field($campaign, 'channelName', ['autoPlaceholder' => false])->label("Channel Name"); ?>
+            <?= $form->field($campaign, 'channelName', ['autoPlaceholder' => false])->input('text', ['value' => \Yii::$app->user->identity->uniqueName])->label("Channel Name"); ?>
         </div>
 
         <!-- if Team Channel -->
