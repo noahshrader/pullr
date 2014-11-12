97ae0aa66677fc99125d172222a50298260f08eb
Noah Shrader <noahshrader@gmail.com>
2014-11-11 21:54:43 -0600

    modal improvements

--- frontend/views/settings/deactivate.php
+++ frontend/views/settings/deactivate.php
@@ -7,28 +7,26 @@ $deactivateAccount = new DeactivateAccount();
 
 <!-- Modal -->
 <div class="modal fade" id="deactivateModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <!-- Deactivation Form -->
-      <? $form = ActiveForm::begin([
-        'action'=> 'app/settings/deactivate' ,
-        'fieldConfig' => ['autoPlaceholder'=>true],
-        'enableAjaxValidation' => true,
-      ]) ?>
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-      </div>
-      <div class="modal-body">
-          <?= $form->field($deactivateAccount, 'reason')->textarea();?>
-          <?= $form->field($deactivateAccount, 'password')->passwordInput() ?>
-          Keep in mind that you have 30 days before the account is completely removed. Contact us if there's anything we can do to change your mind.
-      </div>
-      <div class="modal-footer">
-        <button type="submit" class="btn btn-primary">Deactivate</button>
-        <button type="button" class="btn btn-default" data-dismiss="modal">Nevermind</button>
-      </div>
-      <? ActiveForm::end() ?>
-        <!-- /Deactivation Form -->
-    </div>
-  </div>
+	<div class="modal-dialog module">
+		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+		<h5 class="module-title">Deactivate My Account</h5>
+		<div class="modal-content">
+			<!-- Deactivation Form -->
+			<? $form = ActiveForm::begin([
+				'action'=> 'app/settings/deactivate' ,
+				'fieldConfig' => ['autoPlaceholder'=>true],
+				'enableAjaxValidation' => true,
+			]) ?>
+			<div class="modal-body">
+				<?= $form->field($deactivateAccount, 'reason')->textarea();?>
+				<?= $form->field($deactivateAccount, 'password')->passwordInput() ?>
+			</div>
+			<div class="modal-footer btn-container">
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+				<button type="submit" class="btn btn-primary">Deactivate</button>
+			</div>
+			<? ActiveForm::end() ?>
+				<!-- /Deactivation Form -->
+		</div>
+	</div>
 </div>
--- frontend/views/settings/deactivatepro.php
+++ frontend/views/settings/deactivatepro.php
@@ -5,20 +5,19 @@
 
 <!-- Modal -->
 <div class="modal fade" id="deactivateproModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog module">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h5 class="module-title">Downgrade Your Plan</h5>
         <div class="modal-content">
             <? $form = ActiveForm::begin([
                 'action'=> '/app/settings/deactivatepro'
             ]) ?>
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-            </div>
             <div class="modal-body">
                 <?= $form->field($deactPro, 'reasonId')->dropDownList($deactPro->getReasons());?>
             </div>
-            <div class="modal-footer">
+            <div class="modal-footer btn-container">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
                 <button type="submit" class="btn btn-primary">Don't Renew</button>
-                <button type="button" class="btn btn-default" data-dismiss="modal">Nevermind</button>
             </div>
             <? ActiveForm::end() ?>
         </div>
