ee1036d5f963f050380c4d77087d95075db96cda
Noah Shrader <noahshrader@gmail.com>
2014-11-12 10:43:12 -0600

    streamboard fixes

--- frontend/views/streamboard/region/regions.php
+++ frontend/views/streamboard/region/regions.php
@@ -16,42 +16,40 @@ $class = 'regionsNumber' . $regionsNumber;
          resizable
          resizable-config="{animate: false, handles:'n', create: onRegionResizeCreate, stop: onRegionResizeStop}"
          resizable-condition="region.regionNumber == 2"
-
-         >
+        >
         <div ng-show="region.toShow.alert.message && (region.widgetType == 'widget_alerts')"
             class='widget-alerts'
-            ng-class='region.toShow.alert.animationDirection'
             interaction
-                    draggable
-                    draggable-widget="region.widgetAlerts" 
-                    draggable-region="region" 
-                    draggable-config="{containment:getContainmentByRegion(region) }"
-                    draggable-fields="{widgetLeftAttribute:'positionX', widgetTopAttribute:'positionY'}" 
-                    resizable
-                    resizable-config="{containment: getContainmentByRegion(region) }"
-                    resizable-callback="onResizeAlertImage"                    
-                    resizable-region="region",
-                    resizable-size="{width:region.widgetAlerts.imageWidth, height: region.widgetAlerts.imageHeight}"
-                    >
+            draggable
+            draggable-widget="region.widgetAlerts" 
+            draggable-region="region" 
+            draggable-config="{containment:getContainmentByRegion(region) }"
+            draggable-fields="{widgetLeftAttribute:'positionX', widgetTopAttribute:'positionY'}" 
+            resizable
+            resizable-config="{containment: getContainmentByRegion(region), aspectRatio: true}"
+            resizable-callback="onResizeAlertImage"     
+            resizable-region="region" 
+            resizable-size="{width:region.widgetAlerts.imageWidth, height: region.widgetAlerts.imageHeight}"
+            >
             <!-- if alert message is shown -->
              
-                <img ng-src="{{region.toShow.alert.image}}" 
-                    ng-show='region.toShow.alert.image != null'
-                    ng-hide='region.toShow.alert.preference.hideAlertImage' 
-                    
-                    />
-          
-            <div 
-                    ng-style="{'color': region.toShow.alert.preference.fontColor, 'font-size': region.toShow.alert.preference.fontSize, 'font-family': region.toShow.alert.preference.fontStyle, 'position':'absolute'}" 
-                    ng-hide="region.toShow.alert.preference.hideAlertText"
-                    draggable-widget="region.widgetAlerts" 
-                    draggable-region="region" 
-                    draggable-config="{containment:getContainmentByRegion(region) }"
-                    draggable-fields="{widgetLeftAttribute:'messagePositionX', widgetTopAttribute:'messagePositionY'}" 
-                    interaction
-                    draggable>
-                {{region.toShow.alert.message}}
-            </div>
+            <img ng-src="{{region.toShow.alert.image}}" 
+                ng-show='region.toShow.alert.image != null'
+                ng-hide='region.toShow.alert.preference.hideAlertImage' 
+                ng-class='region.toShow.alert.animationDirection'
+            />
+        </div>
+        <div 
+            ng-style="{'color': region.toShow.alert.preference.fontColor, 'font-size': region.toShow.alert.preference.fontSize, 'font-family': region.toShow.alert.preference.fontStyle, 'position':'absolute'}" 
+            ng-class='region.toShow.alert.animationDirection' 
+            ng-hide="region.toShow.alert.preference.hideAlertText"
+            draggable-widget="region.widgetAlerts" 
+            draggable-region="region" 
+            draggable-config="{containment:getContainmentByRegion(region) }"
+            draggable-fields="{widgetLeftAttribute:'messagePositionX', widgetTopAttribute:'messagePositionY'}" 
+            interaction
+            draggable>
+            {{region.toShow.alert.message}}
         </div>
         <?= $this->render('activity-feed/activity-feed') ?>
         <?= $this->render('campaign-bar/campaign-bar') ?>
