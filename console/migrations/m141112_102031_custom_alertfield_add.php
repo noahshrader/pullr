232af7c2ba2bb44a41d3e9b1d42f0caa78662cf6
Anatoly <tolay199178@gmail.com>
2014-11-12 20:13:53 -0800

    alert text mmigrationi

---
+++ console/migrations/m141112_102031_custom_alertfield_add.php
@@ -0,0 +1,24 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+use frontend\models\streamboard\WidgetAlertsPreference;
+use frontend\models\streamboard\WidgetCampaignBarAlerts;
+
+class m141112_102031_custom_alertfield_add extends Migration
+{
+    const FIELD = 'alertText';
+    public function up()
+    {
+    	$this->addColumn(frontend\models\streamboard\WidgetAlertsPreference::tableName(), self::FIELD, \yii\db\mysql\Schema::TYPE_TEXT . ' NOT NULL');
+        $this->addColumn(frontend\models\streamboard\WidgetCampaignBarAlerts::tableName(), self::FIELD, \yii\db\mysql\Schema::TYPE_TEXT . ' NOT NULL');
+    }
+
+    public function down()
+    {
+        $this->dropColumn(frontend\models\streamboard\WidgetAlertsPreference::tableName(), self::FIELD);
+        $this->dropColumn(frontend\models\streamboard\WidgetCampaignBarAlerts::tableName(), self::FIELD);
+        echo "m141112_102031_custom_alertfield_add cannot be reverted.\n";
+        return false;
+    }
+}
