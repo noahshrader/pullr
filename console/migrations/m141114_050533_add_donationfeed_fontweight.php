8b34b8dd704a7ea901738a95f60004a71331c25f
Thuong <dinhthuong02@hotmail.com>
2014-11-14 12:07:55 +0700

    add font-weight for donation feed

---
+++ console/migrations/m141114_050533_add_donationfeed_fontweight.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m141114_050533_add_donationfeed_fontweight extends Migration
+{
+    const FIELD = 'fontWeight';
+    
+    public function up()
+    {
+        $this->addColumn(\frontend\models\streamboard\WidgetDonationFeed::tableName(), self::FIELD, \yii\db\mysql\Schema::TYPE_INTEGER .' NOT NULL  DEFAULT 400 AFTER fontSize');
+    }
+
+    public function down()
+    {
+        echo "m141114_050533_add_donationfeed_fontweight cannot be reverted.\n";
+
+        return false;
+    }
+}
