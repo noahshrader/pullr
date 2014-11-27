b480f5a1231505850d401c36dbbb7f0ff8e99964
Noah Shrader <noahshrader@gmail.com>
2014-11-26 22:29:56 -0600

    streamboard ui tweaks

--- frontend/views/streamboard/config/region/campaign-bar/module-timer.php
+++ frontend/views/streamboard/config/region/campaign-bar/module-timer.php
@@ -2,12 +2,11 @@
 use frontend\models\streamboard\WidgetCampaignBarTimer;
 
 ?>
-<div class="module first">
-    <div class="panel-group timer-module">
+<div class="campaign-timers module first">
+    <div class="panel-group">
         <h5>Timer Type</h5>
         <div ng-init='TIMER_TYPES=<?= json_encode(WidgetCampaignBarTimer::$TIMER_TYPES) ?>'></div>
-        <select class="timerType" ui-select2="{minimumResultsForSearch: -1}" ng-model="module.timerType" ng-change="regionChanged(region)"
-                data-placeholder="Select one...">
+        <select class="timerType" ui-select2="{minimumResultsForSearch: -1}" ng-model="module.timerType" ng-change="regionChanged(region)" data-placeholder="Select one...">
             <option value=""></option>
             <option ng-repeat='timerType in TIMER_TYPES' value="{{timerType}}">
                 {{timerType}}
@@ -15,22 +14,24 @@ use frontend\models\streamboard\WidgetCampaignBarTimer;
         </select>
         <div ng-show="module.timerType == '<?= WidgetCampaignBarTimer::TIMER_TYPE_COUNTDOWN ?>'">
              <div>
-                 <!--countDownFrom begin-->
-                 <div class="dropdown">
-                     From <a class="dropdown-toggle" id="dateTimeDropdown1" role="button" data-toggle="dropdown" data-target="#" href="#">
-                         <div class="input-group"><input type="text" class="form-control input-sm" ng-model="module.countDownFrom"><span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span>
-                         </div>
+                <!--countDownFrom begin-->
+                <div class="dropdown module">
+                    <h5>From</h5>
+                    <a class="dropdown-toggle" id="dateTimeDropdown1" role="button" data-toggle="dropdown" data-target="#" href="#">
+                        <input type="text" class="form-control" ng-model="module.countDownFrom">
+                        <i class="glyphicon glyphicon-calendar"></i>
                      </a>
                      <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel1">
                          <datetimepicker data-ng-model="module.countDownFrom" data-datetimepicker-config="{ dropdownSelector: '#dateTimeDropdown1' }" on-set-time="onSetTimeCountDownFrom"/>
                      </ul>
-                 </div>
-                 <!--countDownFrom end-->
-                 <!--countDownTo begin-->
-                 <div class="dropdown">
-                     To <a class="dropdown-toggle" id="dateTimeDropdown2" role="button" data-toggle="dropdown" data-target="#" href="#">
-                         <div class="input-group"><input type="text" class="form-control input-sm" ng-model="module.countDownTo"><span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span>
-                         </div>
+                </div>
+                <!--countDownFrom end-->
+                <!--countDownTo begin-->
+                <div class="dropdown module">
+                    <h5>To</h5>
+                    <a class="dropdown-toggle" id="dateTimeDropdown2" role="button" data-toggle="dropdown" data-target="#" href="#">
+                         <input type="text" class="form-control" ng-model="module.countDownTo">
+                         <i class="glyphicon glyphicon-calendar"></i>
                      </a>
                      <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel2">
                          <datetimepicker data-ng-model="module.countDownTo" data-datetimepicker-config="{ dropdownSelector: '#dateTimeDropdown2' }" on-set-time="onSetTimeCountDownTo"/>
@@ -39,10 +40,10 @@ use frontend\models\streamboard\WidgetCampaignBarTimer;
                  <!--countDownTo end-->
              </div>
         </div>
-        <div ng-show="module.timerType == '<?= WidgetCampaignBarTimer::TIMER_TYPE_COUNT_UP ?>'">
-            <button class="btn btn-default btn-sm" ng-show="!module.countUpStatus" ng-click="timerCountUpStart(module, region)">Start</button>
-            <button class="btn btn-default btn-sm" ng-show="module.countUpStatus" ng-click="timerCountUpPause(module, region)">Pause</button>
-            <button class="btn btn-default btn-sm" ng-click="timerCountUpReset(module, region)">Reset</button>
+        <div class="timer-controls" ng-show="module.timerType == '<?= WidgetCampaignBarTimer::TIMER_TYPE_COUNT_UP ?>'">
+            <button class="btn start btn-secondary btn-sm" ng-show="!module.countUpStatus" ng-click="timerCountUpStart(module, region)">Start</button>
+            <button class="btn pause btn-secondary btn-sm" ng-show="module.countUpStatus" ng-click="timerCountUpPause(module, region)">Pause</button>
+            <button class="btn reset btn-secondary btn-sm" ng-click="timerCountUpReset(module, region)">Reset</button>
         </div>
     </div>
 </div>
