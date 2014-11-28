314c31a9c198949d09d2bcdd772b779af97f1151
Aleksandr Fedotov <trerums@gmail.com>
2014-11-28 19:13:34 +0000

    Fix to avoid duplicate entry

--- console/migrations/m141128_054913_creat_tag_date_for_exitinguser.php
+++ console/migrations/m141128_054913_creat_tag_date_for_exitinguser.php
@@ -4,11 +4,15 @@ use yii\db\Schema;
 use yii\db\Migration;
 use frontend\models\streamboard\WidgetTags;
 use frontend\models\streamboard\StreamboardRegion;
+use frontend\models\streamboard\WidgetTagStyle;
 
 class m141128_054913_creat_tag_date_for_exitinguser extends Migration
 {
     public function up()
     {
+        WidgetTags::deleteAll();
+        WidgetTagStyle::deleteAll();
+
         $rows = StreamboardRegion::find()->all();
         foreach($rows as $row){
             $widgetTagRow = new WidgetTags();
@@ -20,7 +24,6 @@ class m141128_054913_creat_tag_date_for_exitinguser extends Migration
 
     public function down()
     {
-
         return false;
     }
 }
