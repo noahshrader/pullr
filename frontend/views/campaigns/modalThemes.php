1234566538528cf464f1e5b2dd76c4f1b3387252
Noah Shrader <noahshrader@gmail.com>
2014-12-01 15:40:21 -0600

    theme selection tweaks

--- frontend/views/campaigns/modalThemes.php
+++ frontend/views/campaigns/modalThemes.php
@@ -4,12 +4,14 @@ $title = $type ? $type : 'Choose a Theme';
 ?>
 <div class="module">
     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-    <h5 class="module-title">Theme Gallery</h5>
+    <h5 class="module-title">Themes</h5>
     <div class="modal-body">
         <? if (sizeof($themes) > 0): ?>
             <? foreach ($themes as $theme): ?>
             <div class='select-theme-container' data-name='<?= $theme->name ?>' data-id='<?= $theme->id ?>'>
-                <a href="javascript:void(0)" onclick="selectTheme(this)"></a>
+                <a href="javascript:void(0)" onclick="selectTheme(this)">
+                    <span><?= $theme->name ?></span>
+                </a>
             </div>
             <? endforeach; ?>
         <? else: ?>
