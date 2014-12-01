0ce1e87e02baf07dbd6a8bc2d020fae6d3a6fe40
Noah Shrader <noahshrader@gmail.com>
2014-11-30 22:46:05 -0600

    form visibility ui fixes

--- frontend/views/campaigns/campaign-edit-form.php
+++ frontend/views/campaigns/campaign-edit-form.php
@@ -6,35 +6,32 @@
     <div class="module-inner">
         <h5><i class="icon mdi-action-toc"></i>Form</h5>
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-4">
                 <!-- Donor Comments -->
                 <div class="form-group">
                     <label>Donor Comments <i class="icon mdi-action-help" data-toggle="tooltip" data-placement="right" title="Toggle OFF to disallow donor comments on your donation form."></i></label>
                     <?= $form->field($campaign, 'enableDonorComments', ['autoPlaceholder' => false])->checkbox([], false); ?>
                 </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-4">
                 <!-- Enable\disable donation form progress bar -->
                 <div class="form-group">
                     <label>Progress Bar <i class="icon mdi-action-help" data-toggle="tooltip" data-placement="right" title="Toggle OFF to hide the progress bar on your donation form."></i></label>
                     <?= $form->field($campaign, 'enableDonationProgressBar', ['autoPlaceholder' => false])->checkbox([], false); ?>
                 </div>
             </div>
-        </div>
-        <!-- Donate Button Text -->
-        <div class="form-group float">
-            <?=$form->field($campaign, 'donationButtonText', ['autoPlaceholder' => false])->label('Donate Button Text')->input('text')->textInput(array('placeholder' => 'What text do you want on your donate button?')); ?>
-        </div>
-        <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-4">
                 <!-- Form Visibility -->
-                <div class="form-group field-campaign-formvisibility">
+                <div class="form-group">
                     <label>Form Visibility <i class="icon mdi-action-help" data-toggle="tooltip" data-placement="right" title="Select 'Visible' if you want to show your form. Select 'Hidden' to hide your form."></i></label>
                     <?= $form->field($campaign, 'formVisibility', ['autoPlaceholder' => false])->checkbox([], false); ?> 
                 </div>        
             </div>
         </div>
-        
+        <!-- Donate Button Text -->
+        <div class="form-group float">
+            <?=$form->field($campaign, 'donationButtonText', ['autoPlaceholder' => false])->label('Donate Button Text')->input('text')->textInput(array('placeholder' => 'What text do you want on your donate button?')); ?>
+        </div>
     </div>
     <div class="module-inner center">
         <h5><i class="icon mdi-action-done-all"></i>Thank You Page</h5>
