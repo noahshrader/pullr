6a26e98ed11d0ea28bb4a6c80f76c3f6f45a5699
Nam Tran <nam.trankhanh.vn@gmail.com>
2014-12-19 10:12:45 +0700

    merge api

--- frontend/views/api/statistic.js
+++ frontend/views/api/statistic.js
@@ -3,24 +3,19 @@ PullrStatistic.MAIN_URL = "//<?=$_SERVER['HTTP_HOST']?><?= \Yii::$app->urlManage
 PullrStatistic.ANGULAR_LIB_URL = PullrStatistic.MAIN_URL + 'public/angular-1.3.4.min.js';
 PullrStatistic.API_URL = PullrStatistic.MAIN_URL + 'app/statistic';
 
+PullrStatistic.LAYOUT_TYPE_SINGLE = "<? echo common\models\Campaign::LAYOUT_TYPE_SINGLE; ?>";
+PullrStatistic.LAYOUT_TYPE_TEAM = "<? echo common\models\Campaign::LAYOUT_TYPE_TEAM; ?>";
+PullrStatistic.LAYOUT_TYPE_MULTI = "<? echo common\models\Campaign::LAYOUT_TYPE_MULTI; ?>";
 
-PullrStatistic.init = function() {
-    var script = document.createElement("script");
-    script.type = 'text/javascript';
-    script.src = PullrStatistic.ANGULAR_LIB_URL;
-    script.async = true;
-    script.onload = function(){
-        PullrStatistic.loadStatsticApp();
-    }
-    document.getElementsByTagName('head')[0].appendChild(script);
+PullrStatistic.bootstrapApp = function(){
+    angular.element(document).ready(function() {
+        angular.bootstrap(document, ['PullrStatisticApp']);
+    });
 }
 
 PullrStatistic.loadStatsticApp = function() {
     var html = document.getElementsByTagName('html')[0];
     html.dataset.ngApp = 'PullrStatisticApp';
-
-    var body = document.getElementsByTagName('body')[0];
-    body.dataset.ngController = 'PullrStatisticCtrl';
 
     var app = angular.module('PullrStatisticApp', []);
 
@@ -67,9 +62,80 @@ PullrStatistic.loadStatsticApp = function() {
         }
     });
 
-    angular.element(document).ready(function() {
-        angular.bootstrap(document, ['PullrStatisticApp']);
+
+}
+
+PullrStatistic.loadFeaturedCampaignApp = function(){
+
+    var app = angular.module('PullrStatisticApp');
+
+    app.controller('PullrCampaignCtrl', function($scope, $timeout, PullrCampaignData) {
+        $scope.LAYOUT_TYPE_SINGLE = PullrStatistic.LAYOUT_TYPE_SINGLE;
+        $scope.LAYOUT_TYPE_MULTI = PullrStatistic.LAYOUT_TYPE_MULTI;
+        $scope.LAYOUT_TYPE_TEAM = PullrStatistic.LAYOUT_TYPE_TEAM;
+
+        $scope.selectedCampaign = null;
+        $scope.isDataReady = false;
+        $scope.campaignDataService = PullrCampaignData;
+        $scope.campaignDataService.fetchCampaignData();
+        $scope.showStreamWindow = false;
+
+        $scope.$watch('campaignDataService.campaignData', function() {
+            $scope.campaigns = $scope.campaignDataService.campaignData;
+        });
+
+        $scope.showChannelInfo = function(channel) {
+            console.log('showChannel');
+            channel.displayInfo = true;
+
+        }
+
+        $scope.hideChannelInfo = function(channel) {
+            channel.displayInfo = false;
+        }
+
+        $scope.watchChannel = function(campaign, channel) {
+            $scope.selectedCampaign = campaign;
+            $scope.selectedChannel = channel;
+            $scope.showStreamWindow = true;
+            $scope.embedPlayerUrl = '//www.twitch.tv/widgets/live_embed_player.swf?channel=' + channel.name;
+            $scope.hostname = 'hostname=www.twitch.tv&channel=' + channel.name + '&auto_play=true&start_volume=25';
+            $scope.chatUrl = '//twitch.tv/' + channel.name + '/chat?popout=';
+            $timeout(function() {
+                var top = $('#watch_window').offset().top;
+                $("html, body").animate({ scrollTop: top }, 600);
+            }, 50);
+        }
     });
+
+    app.service('PullrCampaignData', function($http) {
+        var Service = this;
+        Service.campaignData = [];
+        Service.fetchCampaignData = function() {
+            $http.post(PullrStatistic.API_URL + '/campaign_data').success(function(data) {
+                Service.campaignData = data;
+            });
+        }
+        return Service;
+    });
+
+    app.filter('trusted', ['$sce', function ($sce) {
+        return function(url) {
+            return $sce.trustAsResourceUrl(url);
+        };
+    }]);
+}
+PullrStatistic.init = function() {
+    var script = document.createElement("script");
+    script.type = 'text/javascript';
+    script.src = PullrStatistic.ANGULAR_LIB_URL;
+    script.async = true;
+    script.onload = function(){
+        PullrStatistic.loadStatsticApp();
+        PullrStatistic.loadFeaturedCampaignApp();
+        PullrStatistic.bootstrapApp();
+    }
+    document.getElementsByTagName('head')[0].appendChild(script);
 }
 
 PullrStatistic.init();
